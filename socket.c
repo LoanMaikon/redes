6933a27a5e1d216f29bd749326e623dff3858bbe
luan <luan.marko2@gmail.com>
2024-04-18 17:15:03 -0300

    woniownbo

--- socket.c
+++ socket.c
@@ -1,40 +1,35 @@
 #include "socket.h"
 
+#include <errno.h>
+
 int create_socket(char *interface_name) {
-    int soquete;
-    struct ifreq ir;
-    struct sockaddr_ll endereco;
-    struct packet_mreq mr;
-
-    soquete = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    int soquete = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if (soquete == -1) {
-        printf("Erro no Socket\n");
+        fprintf(stderr, "Erro ao criar socket: Verifique se você é root!\n");
         exit(-1);
     }
-
-    memset(&ir, 0, sizeof(struct ifreq));
-    memcpy(ir.ifr_name, interface_name, sizeof(*interface_name));
-    if (ioctl(soquete, SIOCGIFINDEX, &ir) == -1) {
-        printf("Erro no ioctl\n");
-        exit(-1);
-    }
-
-    memset(&endereco, 0, sizeof(endereco));
+ 
+    int ifindex = if_nametoindex(interface_name);
+ 
+    struct sockaddr_ll endereco = {0};
     endereco.sll_family = AF_PACKET;
     endereco.sll_protocol = htons(ETH_P_ALL);
-    endereco.sll_ifindex = ir.ifr_ifindex;
-    if (bind(soquete, (struct sockaddr *)&endereco, sizeof(endereco)) == -1) {
-        printf("Erro no bind\n");
+    endereco.sll_ifindex = ifindex;
+    // Inicializa socket
+    if (bind(soquete, (struct sockaddr*) &endereco, sizeof(endereco)) == -1) {
+        fprintf(stderr, "Erro ao fazer bind no socket\n");
         exit(-1);
     }
-
-    memset(&mr, 0, sizeof(mr));
-    mr.mr_ifindex = ir.ifr_ifindex;
+ 
+    struct packet_mreq mr = {0};
+    mr.mr_ifindex = ifindex;
     mr.mr_type = PACKET_MR_PROMISC;
-    if (setsockopt(soquete, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mr, sizeof(mr)) == -1)	{
-        printf("Erro ao fazer setsockopt\n");
+    // Não joga fora o que identifica como lixo: Modo promíscuo
+    if (setsockopt(soquete, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mr, sizeof(mr)) == -1) {
+        fprintf(stderr, "Erro ao fazer setsockopt: "
+            "Verifique se a interface de rede foi especificada corretamente.\n");
         exit(-1);
     }
-
+ 
     return soquete;
 }
