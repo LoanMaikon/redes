4b866afddb5983c41c016b1e635e5deff53948ec
luan <luan.marko2@gmail.com>
2024-04-18 17:16:36 -0300

    oiwenfoiwe

--- socket.h
+++ socket.h
@@ -1,14 +1,8 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
+#include <arpa/inet.h>
 #include <net/ethernet.h>
 #include <linux/if_packet.h>
-#include <linux/if.h>
+#include <net/if.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <dirent.h>
-#include <arpa/inet.h>
 
 int create_socket(char *interface_name);
